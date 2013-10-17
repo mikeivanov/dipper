62b7eb6f8161c6a841e6f4b2e76e1974d2111fa2
Mike Ivanov <me@mikeivanov.com>
2013-10-17 01:43:53 -0400

    Add the `symbol-to-keyword` function.

--- util.lisp
+++ util.lisp
@@ -6,6 +6,7 @@
   (:import-from :metabang-bind :bind)
   (:export :let-or
            :string-to-keyword
+           :symbol-to-keyword
            :alist-get :alist-get-str
            :with-re-match
            :with-accessors-in))
@@ -27,6 +28,9 @@
 
 (defun string-to-keyword (string)
   (make-keyword (string-upcase string)))
+
+(defun symbol-to-keyword (symbol)
+  (string-to-keyword (symbol-name symbol)))
 
 (defun with-re-match* (rx string callback)
   (when-let ((match (nth-value 1 (scan-to-strings rx string))))
