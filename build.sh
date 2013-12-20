c3f056e82d7207dc21631f1fef35f23c70af9004
Mike Ivanov <me@mikeivanov.com>
2013-12-19 20:56:02 -0500

    Upgrade to `buildapp`.

--- build.sh
+++ build.sh
@@ -1,3 +1,17 @@
 #!/bin/bash
 set -e
-sbcl --load sbcl-build.lisp
+quicklisp=${QUICKLISP:-~/quicklisp}
+mkdir -p dist
+
+# prime the quicklisp cache and compile all the
+# dependencies as a separate step
+sbcl --eval '(require "dipper")' --eval '(quit)'
+
+# actually build the thing
+buildapp --output dist/dipper \
+         --asdf-tree $quicklisp/dists/quicklisp/installed \
+         --eval '(push #p"./" asdf:*central-registry*)' \
+         --load-system dipper \
+         --eval '(defun main (&args) (dipper::exec))' \
+         --entry main
+
--- sbcl-build.lisp
+++
@@ -1,8 +0,0 @@
-(in-package :cl)
-(asdf:load-system :dipper)
-
-(defun exec ()
-  (dipper::exec)
-  (sb-ext:exit))
-
-(sb-ext:save-lisp-and-die "dipper" :toplevel #'exec :executable t)
